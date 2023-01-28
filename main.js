87ee1ba799f20808157850f695aa48df4fbee86f
void2610 <izumisan2610@gmail.com>
2023-01-28 13:28:08 +0900

    ini

---
+++ main.js
@@ -0,0 +1,9 @@
+var map = L.map("mapid").setView([35.688544, 139.764692], 18);
+L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png", {
+ attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>",
+}).addTo(map);
+
+//JIG-SAW本社の位置にマーカーを追加する。
+var marker = L.marker([35.688544, 139.764692]).addTo(map);
+//上のマーカーにポップアップを追加する。
+marker.bindPopup("JIG-SAW本社").openPopup();
