2c4e97d0c5bfcb145dbfe451cad8728c76aed9b2
Taisuke Fukuno <fukuno@jig.jp>
2023-01-29 08:18:01 +0900

    インデントをよく使われる2スペースに変更したバージョン

--- main.js
+++ main.js
@@ -1,84 +1,73 @@
 import { XML } from "https://js.sabae.cc/XML.js";
 
 async function getXML(link) {
- return await (await fetch(link, { mode: "cors" })).text();
+  return await (await fetch(link, { mode: "cors" })).text();
 }
 var num;
 while (1) {
- num = prompt("読み込むデータ件数を入力(~2397)");
- if (num > 0 && num < 2397) {
-  break;
- }
+  num = prompt("読み込むデータ件数を入力(~2397)");
+  if (num > 0 && num < 2397) {
+    break;
+  }
 }
-
 class GeoData {
- value = 0;
- lat = 0;
- lng = 0;
-
- constructor(a, n, v) {
-  this.lat = a;
-  this.lng = n;
-  this.value = v;
- }
+  value = 0;
+  lat = 0;
+  lng = 0;
+  constructor(a, n, v) {
+    this.lat = a;
+    this.lng = n;
+    this.value = v;
+  }
 }
 let text = document.getElementById("text");
 function setPin(a, n, v) {
- if (v == 0) {
-  console.log("error");
-  return;
- }
- let color = "green";
- if (v < 3) {
-  color = "red";
- }
-
- var marker = L.marker([n, a], {
-  icon: L.spriteIcon(color),
- }).addTo(map);
-
- //上のマーカーにポップアップを追加する。
- marker.bindPopup(String(v)).openPopup();
-}
-
-let allXML = await getXML("https://geofukui.github.io/jiban-opendata/boring.json");
-
-let all = JSON.parse(allXML);
-
-let alldata = new Array(2398);
-
-let gArray = new Array(2398);
-
-for (let i = 0; i < num; i++) {
- text.innerText = "データロード中... " + (i + 1) + "/" + num;
- alldata[i] = await getXML(all[i].url);
- alldata[i] = XML.toJSON(alldata[i]);
- console.log(i);
- let a, n, v;
- if (!alldata[i].ボーリング情報.コア情報.標準貫入試験) {
-  console.log("no data");
- } else {
-  if (!alldata[i].ボーリング情報.コア情報.標準貫入試験.標準貫入試験_合計打撃回数) {
-   a = Number(alldata[i].ボーリング情報.標題情報.経度緯度情報.経度_度["#text"]) + Number(alldata[i].ボーリング情報.標題情報.経度緯度情報.経度_分["#text"]) / 60 + Number(alldata[i].ボーリング情報.標題情報.経度緯度情報.経度_秒["#text"]) / 3600;
-   n = Number(alldata[i].ボーリング情報.標題情報.経度緯度情報.緯度_度["#text"]) + Number(alldata[i].ボーリング情報.標題情報.経度緯度情報.緯度_分["#text"]) / 60 + Number(alldata[i].ボーリング情報.標題情報.経度緯度情報.緯度_秒["#text"]) / 3600;
-   v = Number(alldata[i].ボーリング情報.コア情報.標準貫入試験[0].標準貫入試験_合計打撃回数["#text"]);
+  if (v == 0) {
+    console.log("error");
+    return;
   }
- }
- gArray[i] = new GeoData(Number(a), Number(n), Number(v));
- //console.log("緯度 " + a + " 経度 " + n + " n値 " + v);
+  let color = "green";
+  if (v < 3) {
+    color = "red";
+  }
+  var marker = L.marker([
+    n,
+    a
+  ], { icon: L.spriteIcon(color) }).addTo(map);
+  //上のマーカーにポップアップを追加する。
+  marker.bindPopup(String(v)).openPopup();
 }
-
-var map = L.map("mapid").setView([35.688545, 139.764693], 18);
-L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png", {
- attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>",
-}).addTo(map);
-
-L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.3.1/dist/images/";
-
+let allXML = await getXML("https://geofukui.github.io/jiban-opendata/boring.json");
+let all = JSON.parse(allXML);
+let alldata = new Array(2398);
+let gArray = new Array(2398);
 for (let i = 0; i < num; i++) {
- console.log(gArray[i]);
- if (!gArray[i].lat) {
-  continue;
- }
- setPin(gArray[i].lat, gArray[i].lng, gArray[i].value);
+  text.innerText = "データロード中... " + (i + 1) + "/" + num;
+  alldata[i] = await getXML(all[i].url);
+  alldata[i] = XML.toJSON(alldata[i]);
+  console.log(i);
+  let a, n, v;
+  if (!alldata[i].ボーリング情報.コア情報.標準貫入試験) {
+    console.log("no data");
+  } else {
+    if (!alldata[i].ボーリング情報.コア情報.標準貫入試験.標準貫入試験_合計打撃回数) {
+      a = Number(alldata[i].ボーリング情報.標題情報.経度緯度情報.経度_度["#text"]) + Number(alldata[i].ボーリング情報.標題情報.経度緯度情報.経度_分["#text"]) / 60 + Number(alldata[i].ボーリング情報.標題情報.経度緯度情報.経度_秒["#text"]) / 3600;
+      n = Number(alldata[i].ボーリング情報.標題情報.経度緯度情報.緯度_度["#text"]) + Number(alldata[i].ボーリング情報.標題情報.経度緯度情報.緯度_分["#text"]) / 60 + Number(alldata[i].ボーリング情報.標題情報.経度緯度情報.緯度_秒["#text"]) / 3600;
+      v = Number(alldata[i].ボーリング情報.コア情報.標準貫入試験[0].標準貫入試験_合計打撃回数["#text"]);
+    }
+  }
+  gArray[i] = new GeoData(Number(a), Number(n), Number(v));  //console.log("緯度 " + a + " 経度 " + n + " n値 " + v);
+}
+var map = L.map("mapid").setView([
+  35.688545,
+  139.764693
+], 18);
+L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png", { attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>" }).addTo(map);
+L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.3.1/dist/images/";
+for (let i = 0; i < num; i++) {
+  console.log(gArray[i]);
+  if (!gArray[i].lat) {
+    continue;
+  }
+  setPin(gArray[i].lat, gArray[i].lng, gArray[i].value);
 }
